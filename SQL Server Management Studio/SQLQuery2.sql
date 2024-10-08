70512b6c6ddaec4223acc76db18e2881ce7b29e4
Thiago Feitoza <145472857+thiagofeitoza05@users.noreply.github.com>
2024-10-08 17:56:22 -0300

    Update SQLQuery2.sql

--- SQL Server Management Studio/SQLQuery2.sql
+++ SQL Server Management Studio/SQLQuery2.sql
@@ -8,39 +8,39 @@ USE exerciciocontraints
 GO
 CREATE TABLE editora (
 id_editora				INT				NOT NULL	IDENTITY(491,16),
-nome					VARCHAR(70)		NOT NULL	UNIQUE,
-telefone				VARCHAR(11)		NOT NULL	UNIQUE	CHECK(telefone = 10),
-logradouro_endereco		VARCHAR(200)	NOT NULL,
-numero_endereco			INT				NOT NULL	CHECK(numero_endereco > 0),
-cep_endereco			CHAR(8)			NOT NULL	CHECK(cep_endereco = 8),
-complemento_endereco	VARCHAR(255)	NOT NULL
+nome					VARCHAR(70)			NOT NULL	UNIQUE,
+telefone				VARCHAR(11)			NOT NULL	UNIQUE	CHECK(telefone = 10),
+logradouro_endereco			VARCHAR(200)			NOT NULL,
+numero_endereco				INT				NOT NULL	CHECK(numero_endereco > 0),
+cep_endereco				CHAR(8)				NOT NULL	CHECK(cep_endereco = 8),
+complemento_endereco			VARCHAR(255)			NOT NULL
 PRIMARY KEY (id_editora)
 )
 GO
 CREATE TABLE edicao (
 isbn			CHAR(13)		NOT NULL	CHECK(isbn = 13),
-preco			DECIMAL(4,2)	NOT NULL	CHECK(preco > 0),
-ano				INT				NOT NULL	CHECK(ano > 1993),
-numero_paginas	INT				NOT NULL	CHECK(numero_paginas > 15),
-qnt_estoque		INT				NOT NULL
+preco			DECIMAL(4,2)		NOT NULL	CHECK(preco > 0),
+ano			INT			NOT NULL	CHECK(ano > 1993),
+numero_paginas		INT			NOT NULL	CHECK(numero_paginas > 15),
+qnt_estoque		INT			NOT NULL
 PRIMARY KEY	(isbn)
 )
 GO
 CREATE TABLE autor (
 id_autor	INT				NOT NULL	IDENTITY(2351,1),
-nome		VARCHAR(100)	NOT NULL	UNIQUE,
-data_nasc	DATE			NOT NULL,
-pais_nasc	VARCHAR(50)		NOT NULL	CHECK(pais_nasc = 'Brasil' OR pais_nasc = 'Estados Unidos' OR 
+nome		VARCHAR(100)			NOT NULL	UNIQUE,
+data_nasc	DATE				NOT NULL,
+pais_nasc	VARCHAR(50)			NOT NULL	CHECK(pais_nasc = 'Brasil' OR pais_nasc = 'Estados Unidos' OR 
 													pais_nasc = 'Inglaterra' OR pais_nasc = 'Alemanha'),
-biografia	VARCHAR(255)	NOT NULL
+biografia	VARCHAR(255)			NOT NULL
 PRIMARY KEY (id_autor)
 )
 GO
 CREATE TABLE livroo (
 codigo		INT				NOT NULL	IDENTITY(100001,100),		
-nome		VARCHAR(200)	NOT NULL,
-lingua		VARCHAR(10)		NOT NULL	DEFAULT('PT-BR'),
-ano			INT				NOT NULL	CHECK(ano > 1990),
+nome		VARCHAR(200)			NOT NULL,
+lingua		VARCHAR(10)			NOT NULL	DEFAULT('PT-BR'),
+ano		INT				NOT NULL	CHECK(ano > 1990),
 PRIMARY KEY (codigo)
 )
 GO
@@ -54,7 +54,7 @@ FOREIGN KEY (codigo) REFERENCES livroo(codigo)
 GO
 CREATE TABLE editora_edicao_livroo (
 codigo		INT			NOT NULL,
-isbn		CHAR(13)	NOT NULL,
+isbn		CHAR(13)		NOT NULL,
 id_editora	INT			NOT NULL
 PRIMARY KEY (codigo, isbn, id_editora)
 FOREIGN KEY (codigo) REFERENCES livroo(codigo),
